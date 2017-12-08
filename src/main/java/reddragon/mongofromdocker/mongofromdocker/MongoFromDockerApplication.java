584b19d5535e769c6f3597859dc6caf420363407
Janusz Stolorz <jstolorz@gmail.com>
2017-12-08 14:55:50 +0100

    Init app.

---
+++ src/main/java/reddragon/mongofromdocker/mongofromdocker/MongoFromDockerApplication.java
@@ -0,0 +1,12 @@
+package reddragon.mongofromdocker.mongofromdocker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongoFromDockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MongoFromDockerApplication.class, args);
+	}
+}
---
+++ src/test/java/reddragon/mongofromdocker/mongofromdocker/MongoFromDockerApplicationTests.java
@@ -0,0 +1,16 @@
+package reddragon.mongofromdocker.mongofromdocker;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MongoFromDockerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
